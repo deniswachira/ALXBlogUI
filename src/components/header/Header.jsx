99fd06a57a3d4a66f286381c95ca488129462cfa
Denis Wachira <deniswachira77@gmail.com>
2023-03-26 01:42:19 +0300

    u&i done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div className='header'>
       <div className="headerTitles">
         <span className="headerTitleSm">Blogging without limits</span>
-        <span className="headerTitleLg">Blog</span>
+        <span className="headerTitleLg">Blogify</span>
       </div>
       <img alt=".." className="headerImg" src="https://images.unsplash.com/photo-1653284086534-0895e2fe0abf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8Ul9GeW4tR3d0bHd8fGVufDB8fHx8?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
     </div>
